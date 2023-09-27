c98d2cc0538164c16362022d8ee50c96350f6be5
Russellkusuma <russellkusuma11@gmail.com>
2023-09-26 20:57:22 -0700

    Startup Landing Page Prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import funnyImage from './smurf.jpg';
 
-function App() {
+const Button = ({ link, children }) => (
+  <a href={link} target="_blank" rel="noopener noreferrer" className="App-button">
+    {children}
+  </a>
+);
+
+const Header = () => (
+  <header className="App-header">
+    <h1>Welcome to ScriptCraft</h1>
+    <p>We are using generative AI to assist producers in adapting books for movies, shows, plays, and more.</p>
+  </header>
+);
+
+const WaitlistForm = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Email: ${email}\nAdded to the waitlist!`);
+    setEmail('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="WaitlistForm">
+      <label>
+        Join our waitlist:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="EmailInput"
+        />
+      </label>
+      <input type="submit" value="Submit" className="SubmitButton" />
+    </form>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <WaitlistForm />
+      <img src={funnyImage} alt="Funny" className="FunnyImage" />
+      <div className="App-links">
+        <Button link="https://www.youtube.com/yourchannel">YouTube</Button>
+        <Button link="https://discord.gg/yourdiscord">Discord</Button>
+      </div>
     </div>
   );
 }
